16f5f2a46ca197c57792f66ab643b3e1b57d33bf
Anatoly Glinkin <tolyanbigam@gmail.com>
2024-01-18 12:41:51 +0300

    тесты проверки класса bun

---
+++ tests/test_bun.py
@@ -0,0 +1,27 @@
+import pytest
+
+from bun import Bun
+
+
+class TestBooksCollector:
+    @pytest.mark.parametrize('name,price',
+                             [
+                                 ['Test', 300],
+                                 ['', 300]
+                             ])
+    def test_get_name(self, name, price):
+        # создаем экземпляр (объект) класса Bun
+        bun = Bun(name=name, price=price)
+
+        assert bun.get_name() == name
+
+    @pytest.mark.parametrize('name,price',
+                             [
+                                 ['Test', 300],
+                                 ['Test', 300.9]
+                             ])
+    def test_get_price(self, name, price):
+        # создаем экземпляр (объект) класса Bun
+        bun = Bun(name=name, price=price)
+
+        assert bun.get_price() == price
