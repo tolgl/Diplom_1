11862f32ab31d1e80cf0638e193f94785cb1cd70
Anatoly Glinkin <tolyanbigam@gmail.com>
2024-01-25 18:13:18 +0300

    тесты метода remove_ingredient

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -8,7 +8,7 @@ class TestBurger:
         burger = Burger()
         burger.set_buns(mock_bun)
 
-        assert burger.bun.name == "Bun 1"
+        assert burger.bun.name == "white bun"
         assert burger.bun.price == 50
 
     def test_add_ingredient_with_sauces(self, mock_sauce):
@@ -44,3 +44,35 @@ class TestBurger:
         assert len(burger.ingredients) == 2
         assert burger.ingredients[0].type == 'sauce'
         assert burger.ingredients[1].type == 'filling'
+
+    def test_remove_ingredient_removing_first_ingredient_from_two(self, mock_sauce, mock_filling):
+        # создаем экземпляр (объект) класса Burger
+        burger = Burger()
+        # добавляем 2 ингредиента
+        burger.add_ingredient(mock_sauce)
+        burger.add_ingredient(mock_filling)
+        # удаляем ингредиент
+        burger.remove_ingredient(0)
+
+        assert len(burger.ingredients) == 1
+
+    def test_remove_ingredient_removing_last_ingredient_from_two(self, mock_sauce, mock_filling):
+        # создаем экземпляр (объект) класса Burger
+        burger = Burger()
+        # добавляем 2 ингредиента
+        burger.add_ingredient(mock_sauce)
+        burger.add_ingredient(mock_filling)
+        # удаляем ингредиент
+        burger.remove_ingredient(-1)
+
+        assert len(burger.ingredients) == 1
+
+    def test_remove_ingredient_removing_all_ingredients(self, mock_sauce):
+        # создаем экземпляр (объект) класса Burger
+        burger = Burger()
+        # добавляем 2 соуса
+        burger.add_ingredient(mock_sauce)
+        # удаляем ингредиент
+        burger.remove_ingredient(0)
+
+        assert len(burger.ingredients) == 0
