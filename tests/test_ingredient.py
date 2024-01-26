a0338fe1dd187f2e93323bf768369eddadb9a747
Anatoly Glinkin <tolyanbigam@gmail.com>
2024-01-26 11:57:18 +0300

    добавлена параметризация

--- tests/test_ingredient.py
+++ tests/test_ingredient.py
@@ -1,28 +1,33 @@
+import pytest
+
 from ingredient import Ingredient
 
 
 class TestIngredient:
 
-    def test_get_price(self):
+    @pytest.mark.parametrize('price', [10, 10.99])
+    def test_get_price(self, price):
         # создаем экземпляр (объект) класса Ingredient
         ingredient = Ingredient(ingredient_type='sauce',
                                 name='ketchup',
-                                price=10.5)
+                                price=price)
 
-        assert ingredient.get_price() == 10.5
+        assert ingredient.get_price() == price
 
-    def test_get_name(self):
+    @pytest.mark.parametrize('name', ['hot sauce', 'cutlet'])
+    def test_get_name(self, name):
         # создаем экземпляр (объект) класса Ingredient
         ingredient = Ingredient(ingredient_type='sauce',
+                                name=name,
+                                price=10.5)
+
+        assert ingredient.get_name() == name
+
+    @pytest.mark.parametrize('ingredient_type', ['sauce', 'filling'])
+    def test_get_type(self, ingredient_type):
+        # создаем экземпляр (объект) класса Ingredient
+        ingredient = Ingredient(ingredient_type=ingredient_type,
                                 name='ketchup',
                                 price=10.5)
 
-        assert ingredient.get_name() == 'ketchup'
-
-    def test_get_type(self):
-        # создаем экземпляр (объект) класса Ingredient
-        ingredient = Ingredient(ingredient_type='sauce',
-                                name='ketchup',
-                                price=10.5)
-
-        assert ingredient.get_type() == 'sauce'
+        assert ingredient.get_type() == ingredient_type
